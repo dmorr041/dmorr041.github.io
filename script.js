cdb66aff3b89c2286ea99701d0d3f60afa090cb1
dmorr041 <quinncodes92@gmail.com>
2023-11-12 23:19:59 -0600

    Added background information - small summary of my background and hobbies. Added animations, but they're a bit jank depending on the state of the classes for the content. Need to refine

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ document
     event.preventDefault()
 
     var mainContent = document.getElementById('main-content')
+    var backgroundContent = document.getElementById('background-content')
+    var galleryContent = document.getElementById('gallery-content')
+
     mainContent.classList.add('fade-out')
+    backgroundContent.classList.add('fade-out')
 
     setTimeout(function () {
       mainContent.style.display = 'none'
+      backgroundContent.style.display = 'none'
 
-      var galleryContent = document.getElementById('gallery-content')
       galleryContent.style.display = 'block'
+      galleryContent.classList.remove('fade-out')
       galleryContent.classList.add('slide-in')
     }, 1000)
   })
@@ -21,14 +26,40 @@ document
     event.preventDefault()
 
     var galleryContent = document.getElementById('gallery-content')
+    var backGroundContent = document.getElementById('background-content')
     galleryContent.classList.add('fade-out')
+    backGroundContent.classList.add('fade-out')
 
     setTimeout(function () {
       galleryContent.style.display = 'none'
       galleryContent.classList.remove('fade-out')
+      backGroundContent.style.display = 'none'
+      backGroundContent.classList.remove('fade-out')
 
       var mainContent = document.getElementById('main-content')
       mainContent.style.display = 'block'
       mainContent.classList.add('fade-in')
+    }, 1000)
+  })
+
+document
+  .getElementById('background-link')
+  .addEventListener('click', function (event) {
+    event.preventDefault()
+
+    var mainContent = document.getElementById('main-content')
+    var galleryContent = document.getElementById('gallery-content')
+    var backgroundContent = document.getElementById('background-content')
+
+    mainContent.classList.add('fade-out')
+    galleryContent.classList.add('fade-out')
+
+    setTimeout(function () {
+      mainContent.style.display = 'none'
+      galleryContent.style.display = 'none'
+
+      backgroundContent.style.display = 'block'
+      backgroundContent.classList.remove('fade-out')
+      backgroundContent.classList.add('fade-in')
     }, 1000)
   })
