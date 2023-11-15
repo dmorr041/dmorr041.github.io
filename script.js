2b1271e9a679dd30d5e142d9bf14d5c674b33956
dmorr041 <quinncodes92@gmail.com>
2023-11-15 10:00:57 -0600

    Open Research content added with animations

--- script.js
+++ script.js
@@ -7,21 +7,26 @@ document
     const backgroundContent = document.getElementById('background-content')
     const galleryContent = document.getElementById('gallery-content')
     const c360SummaryContent = document.getElementById('carrier-360-summary')
+    const openResearchContent = document.getElementById('open-research-summary')
 
     mainContent.classList = []
     backgroundContent.classList = []
     c360SummaryContent.classList = []
+    openResearchContent.classList = []
     mainContent.classList.add('fade-out')
     backgroundContent.classList.add('fade-out')
     c360SummaryContent.classList.add('fade-out')
+    openResearchContent.classList.add('fade-out')
 
     setTimeout(function () {
       mainContent.style.display = 'none'
       backgroundContent.style.display = 'none'
       c360SummaryContent.style.display = 'none'
+      openResearchContent.style.display = 'none'
       mainContent.classList = []
       backgroundContent.classList = []
       c360SummaryContent.classList = []
+      openResearchContent.classList = []
 
       galleryContent.style.display = 'block'
       galleryContent.classList.add('slide-in')
@@ -37,6 +42,7 @@ document
     var backgroundContent = document.getElementById('background-content')
     var mainContent = document.getElementById('main-content')
     const c360SummaryContent = document.getElementById('carrier-360-summary')
+    const openResearchContent = document.getElementById('open-research-summary')
 
     // console.log('home: ', mainContent.classList)
     // console.log('projects: ', galleryContent.classList)
@@ -45,17 +51,21 @@ document
     galleryContent.classList = []
     backgroundContent.classList = []
     c360SummaryContent.classList = []
+    openResearchContent.classList = []
     galleryContent.classList.add('fade-out')
     backgroundContent.classList.add('fade-out')
     c360SummaryContent.classList.add('fade-out')
+    openResearchContent.classList.add('fade-out')
 
     setTimeout(function () {
       galleryContent.style.display = 'none'
       backgroundContent.style.display = 'none'
       c360SummaryContent.style.display = 'none'
+      openResearchContent.style.display = 'none'
       galleryContent.classList = []
       backgroundContent.classList = []
       c360SummaryContent.classList = []
+      openResearchContent.classList = []
 
       mainContent.style.display = 'block'
       mainContent.classList.add('fade-in')
@@ -71,21 +81,26 @@ document
     const galleryContent = document.getElementById('gallery-content')
     const backgroundContent = document.getElementById('background-content')
     const c360SummaryContent = document.getElementById('carrier-360-summary')
+    const openResearchContent = document.getElementById('open-research-summary')
 
     mainContent.classList = []
     galleryContent.classList = []
     c360SummaryContent.classList = []
+    openResearchContent.classList = []
     mainContent.classList.add('fade-out')
     galleryContent.classList.add('fade-out')
     c360SummaryContent.classList.add('fade-out')
+    openResearchContent.classList.add('fade-out')
 
     setTimeout(function () {
       mainContent.style.display = 'none'
       galleryContent.style.display = 'none'
       c360SummaryContent.style.display = 'none'
+      openResearchContent.style.display = 'none'
       mainContent.classList = []
       galleryContent.classList = []
       c360SummaryContent.classList = []
+      openResearchContent.classList = []
 
       backgroundContent.style.display = 'block'
       backgroundContent.classList.add('fade-in')
@@ -96,16 +111,19 @@ document
   .getElementById('open-research-card')
   .addEventListener('click', function (event) {
     event.preventDefault()
-    console.log('OR')
 
     const galleryContent = document.getElementById('gallery-content')
+    const openResearchContent = document.getElementById('open-research-summary')
 
     galleryContent.classList = []
     galleryContent.classList.add('fade-out')
 
     setTimeout(function () {
-      galleryContent.display = 'none'
+      galleryContent.style.display = 'none'
       galleryContent.classList = []
+
+      openResearchContent.style.display = 'block'
+      openResearchContent.classList.add('fade-in')
     }, 1000)
   })
 
